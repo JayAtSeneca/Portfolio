5d46ff8fbc92648559ede27496b55c6618c81b73
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-12-29 19:05:55 -0500

    updated timeline component

--- components/TimeLine/TimeLine.js
+++ components/TimeLine/TimeLine.js
@@ -7,38 +7,38 @@ import { TimeLineData } from '../../constants/constants';
 const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
 
 const Timeline = () => {
-  const [activeItem, setActiveItem] = useState(0);
-  const carouselRef = useRef();
+  // const [activeItem, setActiveItem] = useState(0);
+  // const carouselRef = useRef();
 
-  const scroll = (node, left) => {
-    return node.scrollTo({ left, behavior: 'smooth' });
-  }
+  // const scroll = (node, left) => {
+  //   return node.scrollTo({ left, behavior: 'smooth' });
+  // }
 
-  const handleClick = (e, i) => {
-    e.preventDefault();
+  // const handleClick = (e, i) => {
+  //   e.preventDefault();
 
-    if (carouselRef.current) {
-      const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
+  //   if (carouselRef.current) {
+  //     const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
       
-      scroll(carouselRef.current, scrollLeft);
-    }
-  }
+  //     scroll(carouselRef.current, scrollLeft);
+  //   }
+  // }
 
-  const handleScroll = () => {
-    if (carouselRef.current) {
-      const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
+  // const handleScroll = () => {
+  //   if (carouselRef.current) {
+  //     const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
 
-      setActiveItem(index);
-    }
-  }
+  //     setActiveItem(index);
+  //   }
+  // }
 
-  useEffect(() => {
-    const handleResize = () => {
-      scroll(carouselRef.current, 0);
-    }
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     scroll(carouselRef.current, 0);
+  //   }
 
-    window.addEventListener('resize', handleResize);
-  }, []);
+  //   window.addEventListener('resize', handleResize);
+  // }, []);
 
   return (
     <Section id="about">
